f2d12a1f6382ab66975287c88526ac0801d38c53
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2014-04-24 14:05:24 +0200

    Primer commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Proyecto Base Django 1.6
+========================
+
+Estructura básica para un proyecto en django 1.6 con algunas utilidades indispensables
---
+++ gp/gp/settings.py
@@ -0,0 +1,72 @@
+# -*- encoding: utf-8 -*-
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+from unipath import Path
+PROJECT_DIR = Path(__file__).ancestor(2)
+MEDIA_ROOT = PROJECT_DIR.child("media")
+STATIC_ROOT = PROJECT_DIR.child("static")
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+TEMPLATE_DIRS = (
+    PROJECT_DIR.child("templates"),
+)
+
+SECRET_KEY = 'zi5y4+ovup(3+fg)jkd=_(oawzmxr+_4&nggjgnncj43jlx%g3'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = (
+    'grappelli',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'unipath',
+    'django_extensions',
+    'south',
+    'crispy_forms',
+
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+GRAPPELLI_ADMIN_TITLE = 'Gestor de Proyectos Baética'
+ROOT_URLCONF = 'gp.urls'
+WSGI_APPLICATION = 'gp.wsgi.application'
+
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+    'django.core.context_processors.request',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': Path(PROJECT_DIR.ancestor(1), 'bd/db.sqlite3'),
+    }
+}
+
+LANGUAGE_CODE = 'es-es'
+TIME_ZONE = 'Europe/Madrid'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
